f85879122adc43fb3989d649152f6544985c64e5
scottpitcher <85075186+scottpitcher@users.noreply.github.com>
2024-09-22 14:17:38 -0400

    Structured/Organised code

--- RLHF_Scripts/rlhf_phase1.py
+++ RLHF_Scripts/rlhf_phase1.py
@@ -26,7 +26,7 @@ device = 'cpu'
 print(f"Device: {device}")
 
 # Hyperparameters
-epsilon = 0.99                       # Initial exploration rate (probability of choosing a random action)
+epsilon = 0.75                       # Initial exploration rate (probability of choosing a random action); low due to human feedback mechanisms
 epsilon_decay = 0.995                # Decay rate for the exploration probability after each episode
 min_epsilon = 0.05                   # Minimum exploration rate to ensure some exploration continues
 gamma = 0.99                         # Discount factor for future rewards in Q-learning
@@ -73,6 +73,8 @@ time.sleep(2)
 # Start MLflow experiment
 mlflow.set_experiment("Pokemon_Platinum_AI_Phase1")
 
+
+# TRAINING LOOP
 print("Beginning training...")
 # Set MLflow tracking
 with mlflow.start_run():
@@ -84,19 +86,27 @@ with mlflow.start_run():
     mlflow.log_param("num_episodes", num_episodes)
 
     # Loop over num_episodes
-    for episode in range(num_episodes):   # Looping over num_episodes
+    for episode in range(num_episodes):
+
+        # Capture initial state
         state = capture_state().convert('RGB').resize((640, 640))
         state = transforms.ToTensor()(state).unsqueeze(0).to(device)
+        
+        # Initialise episode variables
         done = False
         episode_reward = 0
         j = 0
-
         pyautogui.press('5') # Restart from Jubilife City
+
+        # Episode Start
         while not done:
+            # Step action counter
             j += 1
+
+            # ACTION
+            ## Exploration vs Exploitation
             if np.random.rand() <= epsilon:
-                print("Using exploration")
-                # Chooses from the 
+                print("Using exploration") # Chooses from the action map keys
                 action = np.random.choice(list(ACTION_MAP_DIALOGUE.keys())) 
             else:
                 print("Using exploitation")
@@ -104,22 +114,24 @@ with mlflow.start_run():
                 action = torch.argmax(q_values).item()
                 action = REVERSED_ACTION_MAPPING[action]
             
-            # Map the key onto what the user would understand in actual console gameplay
+            ## Map the key onto what the user would understand in actual console gameplay
             print(f"Action: {ACTION_MAP_DIALOGUE[action]}")
 
-            # Bring DeSmuME back into focus for next action
+            ## Window Focus: Bring DeSmuME back into focus for next action
             os.system("osascript -e 'tell application \"DeSmuME\" to activate'")
-
-            # Use .keyDown/.keyUp as opposed to .press as emulator might not detect input
+            ## Execute Action: Use .keyDown/.keyUp as opposed to .press as emulator might not detect input
             pyautogui.keyDown(action)
             pyautogui.keyUp(action)
 
+            # STATE
             next_state = capture_state().convert('RGB').resize((640, 640))
             next_state = transforms.ToTensor()(next_state).unsqueeze(0).to(device)
+
+            # HUMAN FEEDBACK/REWARD
             reward, done = phase1_reward(screenshot=next_state)
-
-            penalty, better_action =get_human_feedback(ACTION_MAP_DIALOGUE[action])
-
+            penalty, better_action = get_human_feedback(ACTION_MAP_DIALOGUE[action])
+            
+            ## Use immediate training from human feedback, if negative
             if penalty < 0:
                 print(f"Human disapproved the action! Applying penalty: {penalty}")
                 reward += penalty  # Override/adjust reward if action was bad
@@ -144,15 +156,19 @@ with mlflow.start_run():
                     except Exception as e:
                         print(f"Error processing current item: {e}")
             
+            ## Update episode reward
             episode_reward += reward
 
-            # Mapping back to action index as this is the format the model understands
+            # REPLAY BUFFER
+            ## Mapping back to action index as this is the format the model understands
             action_index = ACTION_MAPPING[action]
-            # Append both replay buffers with current state
+
+            ## Append both replay buffers with current state
             replay_buffer.append((state, action_index, reward, next_state, done))
             short_term_buffer.append((state, action_index, reward, next_state, done))
             state = next_state
 
+            ## Enter replay_buffer when there are enough examples, then, every 5th step
             if len(replay_buffer) > batch_size and j>=5:
                 print("Entering replay buffer") 
 
